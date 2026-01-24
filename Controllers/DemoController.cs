0db939b614d5077500c9c79f82801fa582b7a79a
Bill SerGio The Infomercial King™ <tvmogul1@yahoo.com>
2026-01-24 14:20:46 -0500

    update

--- Controllers/DemoController.cs
+++ Controllers/DemoController.cs
@@ -8,5 +8,10 @@ namespace DataGridUXDemo.Controllers
         {
             return View();
         }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
     }
 }
